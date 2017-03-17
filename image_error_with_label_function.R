de2575903072e74a9a21e204373733af54797ba8
Levi Lee 17 <llee17@rstudio.amherst.edu>
2017-03-16 20:54:44 -0400

    added txt file about image error; took away label function r-chunk for ch 0

---
+++ image_error_with_label_function.R
@@ -0,0 +1,23 @@
+#error to paste in the file
+
+
+```{r results = "asis"}
+setwd("~/STAT495-Lee/StatComps2017-02")
+label(path = "
+      /amherst.png", 
+      caption = "Amherst logo", 
+      label = "amherst", type = "figure")
+```
+
+# error message
+# Quitting from lines 96-101 (./chap0.Rmd) 
+# 
+# 
+# Quitting from lines 66-67 (./chap0.Rmd) 
+# Error in UseMethod("label") : 
+#   no applicable method for 'label' applied to an object of class "character"
+# Calls: <Anonymous> ... withCallingHandlers -> withVisible -> eval -> eval -> label
+# In addition: Warning message:
+#   In readLines("abstract.Rmd") :
+#   incomplete final line found on 'abstract.Rmd'
+# Execution halted
